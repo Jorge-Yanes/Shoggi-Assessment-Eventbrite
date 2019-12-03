59c36e4b2c7186721b0366d6dda466ce7e290ef7
Jorge Yanes <yanescampayo@gmail.com>
2019-12-03 16:37:23 +0100

    First commit for the class of the pawn piece

---
+++ pawn.py
@@ -0,0 +1,9 @@
+from piece import *
+
+
+class Pawn(Piece):
+    def __init__(self, player, piece_symbol, piece_type, crowned):
+        Piece.__init__(self, player, piece_symbol, piece_type, crowned)
+
+
+ 
