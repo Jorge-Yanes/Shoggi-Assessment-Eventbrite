d82ef09d3483b39b93a304416c9f9dc026e518e1
Jorge Yanes <yanescampayo@gmail.com>
2019-12-08 18:13:28 +0100

    Gold general piece and its test developed

---
+++ gold_general.py
@@ -0,0 +1,28 @@
+from piece import *
+from cell import *
+
+
+class Gold_General(Piece):
+    def __init__(self, piece):
+
+        self.player = piece.player
+        self.piece_symbol = piece.piece_symbol
+        self.crowned = piece.crowned
+
+        Piece.set_piece_symbol("G")
+
+    def move_is_possible(self, start, finish, piece, finish_piece):
+        if start.row == finish.row and start.column == finish.column:
+            return False
+        if abs(start.row - finish.row) <= 1 and abs(start.column - finish.column) <= 1:
+            # Allow backward movement, except for the diagonal, for player 1
+            if piece.player == 1 and start.row-finish.row == 1 and start.column != finish.column:
+                return False
+            # Allow backward movement, except for the diagonal, for player 2
+            elif piece.player == 2 and start.row-finish.row == -1 and start.column != finish.column:
+                return False
+            # Checks if there is a piece in the end position and if its ours
+            if finish_piece != None and piece.player == finish_piece.player:
+                return False
+            return True
+        return False
---
+++ test_gold_general.py
@@ -0,0 +1,50 @@
+import unittest
+from gold_general import *
+from random import randint
+
+class TestClass(unittest.TestCase):
+
+    # Test cases with allowed moves
+    def test_move_is_possible(self):
+        for a in range(8):
+            # Piece that we are going to move
+            piece_1 = Piece(player=1, piece_symbol='P',crowned=False)
+
+            # Piece at the final position
+            finish_piece = None
+
+            # One cell fordward, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            # One cell backward, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a, row=a-1, piece=finish_piece), piece_1, finish_piece))
+ 
+            # One cell to the right, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a+1, row=a, piece=finish_piece), piece_1, finish_piece))
+            
+            # One cell to the left, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a-1, row=a, piece=finish_piece), piece_1, finish_piece))
+
+            # Top-right diagonal, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a+1, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            # Top-left diagonal, allowed movement where the final cell is empty
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a-1, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            # Not allowed movements. 
+            self.assertFalse(Gold_General.move_is_possible(None, Cell(column=a+randint(2,8), row=a+randint(2,8), piece=piece_1), Cell(column=a-1, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            #Diagonal movement where there is an opponent piece
+            finish_piece = Piece(player=2, piece_symbol='P', crowned=False)            
+            self.assertTrue(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a+1, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            #Diagonal movement where there is one of our pieces
+            finish_piece = Piece(player=1, piece_symbol='P',crowned=False)            
+            self.assertFalse(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a+1, row=a+1, piece=finish_piece), piece_1, finish_piece))
+
+            #Starting and finishing cells are the same
+            self.assertFalse(Gold_General.move_is_possible(None, Cell(column=a, row=a, piece=piece_1), Cell(column=a, row=a, piece=finish_piece), piece_1, finish_piece))
+
+
+if __name__ == '__main__':
+    unittest.main()
